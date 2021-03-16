a7c9dca07400304c176b01ed63067bf9bea1bf02
soorichu <soorichu@gmail.com>
2021-03-16 11:50:17 +0900

    Initial commit

---
+++ Assets/RouletteController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0.0f;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {  //마우스 왼쪽 버튼 돌아감
+            this.rotSpeed = 20.0f;
+        }
+
+        if (Input.GetMouseButtonDown(1)) {  //마우스 오른쪽 버튼일 때 멈춤
+            this.rotSpeed = 0.0f;
+        }
+
+        this.rotSpeed *= 0.987f;  //속도가 서서히 줄어 들음.
+        transform.Rotate(0, 0, this.rotSpeed);
+    }
+}
